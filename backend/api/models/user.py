b4ebae6e8d123e14a3247d6e4255ceb92f5f73a1
Aliz <yojan.bustamante@udea.edu.co>
2022-09-10 15:32:56 +0000

    Progreso en el modelo de usuario

---
+++ backend/api/models/user.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class User (models.Model):
+    # Required fields
+    name = models.CharField (max_length=32)
+    email = models.EmailField ()
+    password = models.CharField (max_length=125)
+
+    # With default values fields
+    bio = models.TextField (blank=True, null=True)
+    
+    creation_date = models.DateTimeField (default=timezone.now)
+    is_admin = models.BooleanField (default=False)
+    followers = models.DecimalField (max_digits=10, decimal_places=0, default=0)
