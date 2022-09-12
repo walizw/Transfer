61eb9746167fae2ac0fe8a8b81c82a58d291c306
Aliz <yojan.bustamante@udea.edu.co>
2022-09-12 22:24:55 +0200

    Derp

--- backend/ap/urls.py
+++ backend/ap/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path ("users/<str:name>", views.UserEndpointView.as_view ()),
     path (".well-known/webfinger", views.WebFingerView.as_view ()),
-    path ("users/<str:name>/inbox", viwes.UserInboxView.as_view ()),
+    path ("users/<str:name>/inbox", views.UserInboxView.as_view ()),
     path ("users/<str:name>/outbox", views.UserOutboxView.as_view ())
 ]
