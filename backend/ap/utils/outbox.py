4808d7f73d9f678d1ff8259a935583ff8117e393
Aliz <yojan.bustamante@udea.edu.co>
2022-09-14 01:16:19 +0200

    Creada clase Outbox

---
+++ backend/ap/utils/outbox.py
@@ -0,0 +1,71 @@
+from api.models import User
+
+from ..models import Activity
+from ..utils.federation import Federation
+
+from django.conf import settings
+
+class Outbox ():
+    actor = None
+    data = {}
+    message = {
+        "@context": "https://www.w3.org/ns/activitystreams"
+    }
+    activities = len (Activity.objects.all ())
+    activity = None
+    object = ""
+    
+    def __init__ (self, actor, data, object):
+        self.actor = actor
+        self.data = data
+        self.object = object
+
+        if self.data ['type'] == "Follow":
+            self.process_follow ()
+        elif self.data ['type'] == "Unfollow":
+            self.process_unfollow ()
+        elif self.data ['type'] == "Accept":
+            self.process_accept ()
+        else:
+            return # No hacer nada?
+
+        self.fedi = Federation (self.actor)
+        response = self.fedi.send_one (self.object, self.message)
+
+        if self.activity != None:
+            self.activity.save ()
+
+    def process_follow (self):
+        self.message ["id"] = f"{self.data ['actor']}/follows/{self.activities}"
+        self.message ["type"] = "Follow"
+        self.message ["actor"] = self.data ["actor"]
+        self.message ["object"] = self.data ['object']
+
+        self.activity = Activity (
+            activity_id = self.message["id"],
+            type = "Follow",
+            actor = self.message ["actor"],
+            object = self.message ["object"]
+        )
+
+    def process_unfollow (self):
+        follow_activity = Activity.objects.filter (actor=self.data ["actor"], object=self.object)
+        if len (follow_activity) < 1:
+            return
+        follow_activity = follow_activity.get ()
+
+        self.message ["id"] = f"{self.data ['actor']}/follows/{self.activities}/undo"
+        self.message ["type"] = "Undo"
+        self.message ["actor"] = self.data ["actor"]
+        self.message ["object"] = {
+            "id": follow_activity.activity_id,
+            "type": "Follow",
+            "actor": follow_activity.actor,
+            "object": follow_activity.object
+        }
+
+        print (self.message)
+        # follow_activity.delete ()
+
+    def process_accept (self):
+        pass
--- backend/ap/views/follow.py
+++ backend/ap/views/follow.py
@@ -6,6 +6,7 @@ from api.models import User
 
 from ..models import Activity
 from ..utils.federation import Federation
+from ..utils.outbox import Outbox
 
 import hashlib
 
@@ -20,27 +21,16 @@ class FollowRemoteAPIView (APIView):
         recipient_url = request.POST ["recipient"]
         sender_url = f"{settings.DOMAIN_NAME}/api/v1/users/{username}"
 
-        activity_hash = hashlib.new ("md5", str (len (Activity.objects.all ())).encode ()).digest ()
-        activity_id = f"{sender_url}/follows/{activity_hash}"
-
-        follow_request_message = {
-            "@context": "https://www.w3.org/ns/activitystreams",
-            "id": activity_id,
+        outbox = Outbox (user, {
             "type": "Follow",
             "actor": sender_url,
             "object": recipient_url
-        }
-
-        fedi = Federation (user)
-        response = fedi.send_one (recipient_url, follow_request_message)
-
-        act = Activity (activity_id=activity_id, type="Follow", actor=sender_url, object=recipient_url)
-        act.save ()
+        }, recipient_url)
 
         user.following += 1
         user.save ()
 
-        return Response (response.content.decode ())
+        return Response ()
 
 class UnfollowRemoteAPIView (APIView):
     def post (self, request):
@@ -50,33 +40,13 @@ class UnfollowRemoteAPIView (APIView):
         recipient_url = request.POST ["recipient"]
         sender_url = f"{settings.DOMAIN_NAME}/api/v1/users/{username}"
 
-        activity_hash = hashlib.new ("md5", str (len (Activity.objects.all ())).encode ()).digest ()
-        activity_id = f"{sender_url}/follows/{activity_hash}/undo"
-
-        follow_activity = Activity.objects.filter (actor=sender_url, object=recipient_url)
-        if len (follow_activity) < 1:
-            print (f"The activity where {sender_url} follows {recipient_url} doesn't exist")
-            return Response ("Error")
-        follow_activity = follow_activity.get ()
-
-        follow_request_message = {
-            "@context": "https://www.w3.org/ns/activitystreams",
-            "id": activity_id,
-            "type": "Undo",
+        outbox = Outbox (user, {
+            "type": "Unfollow",
             "actor": sender_url,
-            "object": {
-                "id": follow_activity.activity_id,
-                "type": "Follow",
-                "actor": sender_url,
-                "object": recipient_url
-            }
-        }
-
-        fedi = Federation (user)
-        response = fedi.send_one (recipient_url, follow_request_message)
-        follow_activity.delete ()
+            "object": recipient_url
+        }, recipient_url)
 
         user.following -= 1
         user.save ()
 
-        return Response (response.content.decode ())
+        return Response ()
