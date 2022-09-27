a23ae04ca7da6c56aa98c838e675c15e5aa6edff
walizw <yojan.bustamante@udea.edu.co>
2022-09-26 20:54:41 -0500

    Progreso en el frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Header />
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import "./assets/styles.css";
 </style>
+
+<script>
+    import Header from "./components/Header"
+
+  export default {
+      name: "App",
+      components: {
+	  Header
+      }
+  }
+</script>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+
+    <div class="page-flows">
+      <span class="flow">
+	<ion-icon name="chevron-back-outline"></ion-icon>
+		  <!-- @click="$router.back ()"></ion-icon> -->
+      </span>
+
+      <span class="flow">
+	<ion-icon name="chevron-forward-outline"></ion-icon>
+		  <!-- @click="$router.forward ()"></ion-icon> -->
+      </span>
+    </div>
+    
+  </header>
+</template>
